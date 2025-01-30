581ed63739d3e22b56a3e036544442965b26b9e6
Morten Høfft <6759267+MortenHofft@users.noreply.github.com>
2025-01-30 12:36:53 +0100

    explicitly state enabled routes to make transition easier to new components

--- _includes/js/config.js
+++ _includes/js/config.js
@@ -13,15 +13,11 @@ var siteTheme = gbifReactComponents.themeBuilder.extend({
 
 var siteConfig = {
   routes: {
+    enabledRoutes: ['occurrenceSearch', 'literatureSearch'],
     occurrenceSearch: {
       // The route you are currently using for occurrence search. The language prefix will be added automatically
       // If you need special routes per language, then you have to add locale specific overwrites. The page language is available as a global variable called `pageLang`
       route: '/specimen/search'
-    },
-    literatureSearch: {
-    },
-    collectionSearch: {
-      disabled: true
     }
   },
   literature: {
